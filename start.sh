8226a0e29b5fe44ece45c51c386bbd79b9506050
PmsHere <72794063+PmsHere@users.noreply.github.com>
2024-06-08 13:23:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,21 +1,12 @@
-#!/bin/bash
-
-# Exit immediately if a command exits with a non-zero status
-set -e
-
-# Set the default repository if UPSTREAM_REPO is not set
-: ${UPSTREAM_REPO:="https://github.com/PmsHere/MTTGFilmRoboT"}
-
-# Clone the repository
-echo "Cloning Repository from $UPSTREAM_REPO"
-git clone $UPSTREAM_REPO /MTTGFilmRoboT
-
-# Change directory to the cloned repository
-cd /MTTGFilmRoboT
-
-# Install dependencies
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/VJBots/AutoFilterAdvance /AutoFilterAdvance
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /AutoFilterAdvance
+fi
+cd /AutoFilterAdvance
 pip3 install -U -r requirements.txt
-
-# Start the bot
 echo "Bot Started...."
 python3 bot.py
