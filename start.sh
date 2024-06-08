664c5720679eb98a531cce7d66f1bca49fff7c1d
PmsHere <72794063+PmsHere@users.noreply.github.com>
2024-06-08 12:57:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/PmsHere/MTTGFilmRoboT /MTTGFilmRoboT
+  git clone https://github.com/PmsHere/MTTGFilmRoboT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /MTTGFilmRoboT
