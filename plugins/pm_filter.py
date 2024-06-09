4eda19fc55710a63720aee3de3e3e9d1ca97dbf0
PmsHere <72794063+PmsHere@users.noreply.github.com>
2024-06-09 13:12:23 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -674,11 +674,11 @@ async def auto_filter(client, msg, spoll=False):
             for file in files
         ]
 
-    btn.insert(0,
-        [
-                InlineKeyboardButton("💢 𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗠𝗮𝗶𝗻 𝗰𝗵𝗮𝗻𝗻𝗲𝗹 💢",invite_link),
-        ]
-    )
+    invite_link = f.invite_link
+
+btn.insert(0, [
+    InlineKeyboardButton("💢 𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗠𝗮𝗶𝗻 𝗰𝗵𝗮𝗻𝗻𝗲𝗹 💢", invite_link),
+])
 
     if offset != "":
         key = f"{message.chat.id}-{message.id}"
