e62aa731d47dbe7561c7273fc0da10975cabb63f
PmsHere <72794063+PmsHere@users.noreply.github.com>
2024-06-15 12:18:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -5,7 +5,6 @@ import ast
 import math
 from pyrogram.errors.exceptions.bad_request_400 import MediaEmpty, PhotoInvalidDimensions, WebpageMediaEmpty
 from Script import script
-from fsub import invite_link
 import pyrogram
 from database.connections_mdb import active_connection, all_connections, delete_connection, if_active, make_active, \
     make_inactive
